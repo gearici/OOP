b8827faf08d49db8f3d2b47bc342609ab771992c
Gizem Ece Arıcı <gizemece.arici@gazi.edu.tr>
2021-01-24 14:39:38 +0300

    21OCAK

---
+++ Collections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic; // List yazdıktan sonra soldaki ampulden bunu ekleyerek listi aktive ettik
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin", "Murat", "Kerem", "Halil" };
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //Console.WriteLine(isimler[4]);
+            //bunu yapamayız çünkü diziler en başta tanımladığımız sayının dışına çıkmaz
+
+            //string[] isimler = new string[5];
+            //yukarıdaki işlemi yaparak 5 boyutlu bir dizi oluşturur ve ilkeri yazdırırız ama 0 1 2 3. indekslerdeki elemanları
+            //tanımlamadığımız için isimler[0] yazdırdığımızda boş gelir, yeniden new yapmak yeni bir adres oluşturur!!
+            //isimler[4] = "İlker"; 
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            //bu sebeple dizi kullanmak yerine java ve c#ta koleksiyonlar(List) kullanırız.
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" }; 
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+
+        }
+    }
+}
---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T> //string değil de T değişkeni kullanıcam diyorum kuralları kendim koyuyorum
+                    //hangi değişkenle bu classtan eleman oluşturursak onu benimser
+    {
+        T[] items;
+
+        public MyList() //ctor tab tab yaptık bu bir constructor
+        {
+            items = new T[0]; //bir blok newlendiğinde oluşan bloğa constructor denir
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; //önceki değerleri buna tutturuyoruz yeni değeri alttaki satırda giriyoruz
+            items = new T[items.Length + 1]; //eleman sayısını 1 arttırır ama new yapınca öncekiler uçuyordu bunu birine tutturalım geçici diziye 
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; //geçiçi diziye devrettiğim elemanları items listeme tekrardan alıyorum
+            }
+
+            items[items.Length - 1] = item;//itemda en sonda boş bir hücre kalmıştı ona da son değerini atadık
+        }
+    }
+}
